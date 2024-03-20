68c400672a60b67752f7afcc5e2ed4b5cb4aa062
juanjose999 <juanjose000012@gmail.com>
2024-03-20 14:16:19 -0500

    init proyect

---
+++ src/main/java/com/comidas/rapidas/foodSpeedApplication.java
@@ -0,0 +1,13 @@
+package com.comidas.rapidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class foodSpeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(foodSpeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comidas/rapidas/foodSpeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comidas.rapidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class foodSpeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
